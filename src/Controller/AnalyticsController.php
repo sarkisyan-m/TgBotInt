829f7f5e22bd05723465043b5e2031def931805c
Михаил Саркисян <sarkisyan@intaro.email>
2019-03-12 12:08:52 +0300

    аналитика

--- src/Controller/AnalyticsController.php
+++ src/Controller/AnalyticsController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,7 +15,7 @@ class AnalyticsController extends Controller
      *
      * @param Request $request
      *
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return Response
      */
     public function index(Request $request)
     {
@@ -26,20 +27,147 @@ class AnalyticsController extends Controller
             }
         }
 
-        $paths = $this->getPaths();
+        return $this->render('analytics/analytics.html.twig');
+    }
 
-        $content = [];
-        foreach ($paths as $path) {
-            $content[] = $this->getContent($path);
+    public function sortPositionDistance($x, $y)
+    {
+        return $x['TOTAL_QUANTITY_LACKS'] > $y['TOTAL_QUANTITY_LACKS'] ||
+        $x['TOTAL_QUANTITY_LACKS'] == $y['TOTAL_QUANTITY_LACKS'] && $x['DISTANCE_DIFF'] > $y['DISTANCE_DIFF'] ? 1 :
+            ($x['TOTAL_QUANTITY_LACKS'] < $y['TOTAL_QUANTITY_LACKS'] ? -1 : 0);
+    }
+
+    /**
+     * @Route("/analytics/data", name="analytics_data")
+     *
+     * @param Request $request
+     *
+     * @return JsonResponse|Response
+     */
+    public function data(Request $request)
+    {
+        if ($request->isXmlHttpRequest() || true) {
+
+            $paths = $this->getPaths();
+
+            $content = [];
+            foreach ($paths as $path) {
+                $content[] = $this->getContent($path);
+            }
+
+            $content = array_values(array_filter($content));
+
+            $data = [];
+            foreach ($content as $file) {
+                foreach ($file as $row) {
+
+                    /**
+                     * Date
+                     */
+
+                    $date = new \DateTime($row['datetime']['date']);
+                    $date = $date->format('d/m/Y H:i:s.v');
+
+                    /**
+                     * User
+                     */
+
+                    $user = [
+                        'first_name' => null,
+                        'last_name' => null,
+                        'username' => null,
+                        'name' => null,
+                        'full_name' => null
+
+                    ];
+
+                    if (isset($row['message']['result']['chat'])) {
+                        if (isset($row['message']['result']['chat']['first_name'])) {
+                            $user['first_name'] = $row['message']['result']['chat']['first_name'];
+                        }
+                        if (isset($row['message']['result']['chat']['last_name'])) {
+                            $user['last_name'] = $row['message']['result']['chat']['last_name'];
+                        }
+                        if (isset($row['message']['result']['chat']['username'])) {
+                            $user['username'] = $row['message']['result']['chat']['username'];
+                        }
+                    } elseif (isset($row['message']['callback_query']['from'])) {
+                        if (isset($row['message']['callback_query']['from']['first_name'])) {
+                            $user['first_name'] = $row['message']['callback_query']['from']['first_name'];
+                        }
+                        if (isset($row['message']['callback_query']['from']['last_name'])) {
+                            $user['last_name'] = $row['message']['callback_query']['from']['last_name'];
+                        }
+                        if (isset($row['message']['callback_query']['from']['username'])) {
+                            $user['username'] = $row['message']['callback_query']['from']['username'];
+                        }
+                    } elseif (isset($row['message']['message']['from'])) {
+                        if (isset($row['message']['message']['from']['first_name'])) {
+                            $user['first_name'] = $row['message']['message']['from']['first_name'];
+                        }
+                        if (isset($row['message']['message']['from']['last_name'])) {
+                            $user['last_name'] = $row['message']['message']['from']['last_name'];
+                        }
+                        if (isset($row['message']['message']['from']['username'])) {
+                            $user['username'] = $row['message']['message']['from']['username'];
+                        }
+                    }
+
+                    $user['name'] = implode(' ', array_filter([$user['first_name'], $user['last_name']]));
+
+                    if ($user['username']) {
+                        $user['full_name'] = "{$user['name']} ({$user['username']})";
+                    } else {
+                        $user['full_name'] = $user['name'];
+                    }
+
+                    /**
+                     * Type
+                     */
+
+                    isset($row['message']['callback_query']) ? $type = 'callback' : $type = 'message';
+
+                    /**
+                     * Channel
+                     */
+
+                    $row['channel'] == 'telegram_request_in' ? $channel = 'in' : $channel = 'out';
+
+                    /**
+                     * Text
+                     */
+
+                    $text = null;
+
+                    if (isset($row['message']['result']['text'])) {
+                        $text = $row['message']['result']['text'];
+                    } elseif (isset($row['message']['message']['text'])) {
+                        $text = $row['message']['message']['text'];
+                    } elseif (isset($row['message']['callback_query']['message']['text'])) {
+                        $text = $row['message']['callback_query']['message']['text'];
+                    }
+
+                    /**
+                     * Result
+                     */
+
+                    $data[] = [
+                        'date' => $date,
+                        'user' => $user,
+                        'type' => $type,
+                        'channel' => $channel,
+                        'text' => $text,
+                        'data' => json_encode($row['message'], JSON_UNESCAPED_UNICODE),
+                    ];
+                }
+            }
+
+//            $data = json_encode($data);
+
+            return new JsonResponse($data);
         }
 
-        $content = array_values(array_filter($content));
-
-        dump([$content]);
-
-        return $this->render('analytics/analytics.html.twig', [
-            'content' => $content
-        ]);
+        return new Response('', Response::HTTP_FORBIDDEN);
     }
 
     public function getPaths()
