46a2e7d78e2e937ac99a3f8ced13866faa6ce7d4
Михаил Саркисян <sarkisyan@intaro.email>
2019-03-12 15:48:59 +0300

    аналитика. исправления. работа с куками

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,9 @@ const $ = require('jquery');
 window.$ = $;
 window.jQuery = $;
 
+const util = require('util');
+window.util = util;
+
 require('bootstrap');
 require('datatables');
 require('./main.js');
